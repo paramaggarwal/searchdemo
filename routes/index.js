1ab91f855e680f054d8da0c5668bdf5f70d36fbb
Param Aggarwal <paramaggarwal@gmail.com>
2015-03-20 15:45:56 +0530

    add input field

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,39 @@ var express = require('express');
 var router = express.Router();
 
 var React = require('react');
+var search = require('../clients/search');
 var App = require('../ui');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  var page = React.renderToString(<App />);
-  var props = {};
-  
-  res.render('index', {
-    title: 'Express',
-    page: page,
-    props: props
-  });
+  var term = req.query.q;
+
+  if (term) {
+    search(term, function (err, data) {
+      if (err) {
+        res.status(500).send(err);
+      };
+
+      var props = {
+        data: data
+      };
+
+      res.render('index', {
+        title: 'Search Myntra',
+        page: React.renderToString(<App {...props} />),
+        props: props
+      });
+    });
+
+  } else {
+    var props = {};  
+    res.render('index', {
+      title: 'Search Myntra',
+      page: React.renderToString(<App {...props} />),
+      props: props
+    });
+  }
 });
 
 module.exports = router;
--- ui/index.js
+++ ui/index.js
@@ -5,9 +5,10 @@ var App = React.createClass({
   render: function () {
     return (
       <div>
-        <h1>Hello</h1>
-        <form>
-          <input type='text' /> 
+        <h1>Search Myntra</h1>
+        <form action='/' method='get'>
+          <input type='text' name='q' /> 
+          <input type='submit' value='Search' />
         </form>
       </div>
     );
